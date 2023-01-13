1254cc7cb491411a9e567ce97c19fbf880b7b87e
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-13 03:33:08 +0300

    PopularMovies -> swiperjs integration & component.

--- src/components/movies/PopularMovies.tsx
+++ src/components/movies/PopularMovies.tsx
@@ -2,16 +2,36 @@ import { For } from "solid-js";
 import Card from "./MiniMovieCard";
 import type { ResultPM } from "./types";
 
+import { Swiper, SwiperSlide } from "swiper/solid";
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 export default function PopularMovies(props: { results: ResultPM[] }) {
   return (
-    <ul>
-      <For each={props.results}>
-        {(movie, i) => (
-          <li>
-            <Card title={movie.title} />
-          </li>
-        )}
-      </For>
-    </ul>
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+        }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <For each={props.results}>
+          {(movie, i) => (
+            <SwiperSlide>
+              <Card title={movie.title} />
+            </SwiperSlide>
+          )}
+        </For>
+      </Swiper>
+    </>
   );
 }
