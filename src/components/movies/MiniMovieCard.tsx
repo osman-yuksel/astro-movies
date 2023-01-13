cb16909101dca8a679b402c65b274f547520b1c6
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-13 04:30:27 +0300

    Style changes

--- src/components/movies/MiniMovieCard.tsx
+++ src/components/movies/MiniMovieCard.tsx
@@ -1,11 +1,22 @@
-export default function Card(props: { title: string; backdrop: string }) {
+export default function Card(props: {
+  title: string;
+  backdrop: string;
+  overview: string;
+}) {
   return (
-    <div class="flex w-full flex-col items-center border-2 border-black p-2">
-      <img
-        class=""
-        src={`https://image.tmdb.org/t/p/original/${props.backdrop}`}
-      ></img>
-      <h3 class="w-64 text-center">{props.title}</h3>
+    <div class="flex w-full items-center text-white">
+      <div class="ml-4">
+        <h3 class="w-64 text-xl mb-2">{props.title}</h3>
+        <p>{props.overview}</p>
+      </div>
+      <div>
+        <div class="absolute h-full w-full bg-gradient-to-r from-black via-black/0" />
+        <img
+          class=""
+          src={`https://image.tmdb.org/t/p/original/${props.backdrop}`}
+          alt="Movie backdrop image"
+        />
+      </div>
     </div>
   );
 }
