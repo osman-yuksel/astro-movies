ed20f05b529f1fa0fec0008c25f8e067585b25f2
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-15 04:47:14 +0300

    progress bar added for swiper

--- src/components/movies/BigMovieCard.tsx
+++ src/components/movies/BigMovieCard.tsx
@@ -10,7 +10,7 @@ export default function Card(props: {
       <div class="z-10 ml-14 flex h-full w-4/12 items-center">
         <div>
           <h1 class="mb-4 w-full text-5xl">{props.title}</h1>
-          <div class="flex flex-row text-xl mb-4">
+          <div class="mb-4 flex flex-row text-xl">
             <h3 class="mr-4">{props.rd.slice(0, 4)}</h3>
             <h3>{props.rating}</h3>
           </div>
@@ -18,7 +18,7 @@ export default function Card(props: {
         </div>
       </div>
       <div class="absolute right-0 z-0 h-full w-9/12">
-        <div class="absolute h-full w-full bg-gradient-to-r from-black via-black/0" />
+        <div class="absolute h-full w-full bg-gradient-to-r from-black via-black/40 to-black/0" />
         <picture class="flex h-full justify-end">
           <source
             srcset={`https://image.tmdb.org/t/p/w1280/${props.backdrop}`}
--- src/components/movies/PopularMovies.tsx
+++ src/components/movies/PopularMovies.tsx
@@ -1,25 +1,42 @@
-import { For } from "solid-js";
+import { For, createSignal } from "solid-js";
 import Card from "./BigMovieCard";
 import type { ResultPM } from "./types";
 
 import { Swiper, SwiperSlide } from "swiper/solid";
-import { Navigation, Pagination, Autoplay, Virtual, Lazy } from "swiper";
+import { Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 export default function PopularMovies(props: { results: ResultPM[] }) {
   let swiperRef: any;
+  const delay = 5000;
+  const [progress, setProgress] = createSignal(false);
+  const twclass = () =>
+    progress() ? `w-full duration-[${delay.toString()}ms]` : `w-0`;
+
+  const ProgressHandler = () => {
+    setProgress(false);
+    setTimeout(() => setProgress(true), 0);
+  };
 
   return (
-    <div class="swipper-wrapper">
+    <div
+      class="swipper-wrapper relative"
+      onMouseLeave={() => {
+        swiperRef.autoplay.start();
+      }}
+    >
       <Swiper
         modules={[Autoplay]}
         spaceBetween={0}
         slidesPerView={1}
         loop={true}
-        
-        onSlideChange={() => console.log("slide change")}
+        autoplay={{ delay: delay }}
+        onSlideChange={() => {
+          console.log("slide change");
+          ProgressHandler();
+        }}
         onSwiper={(swiper) => (swiperRef = swiper)}
       >
         <For each={props.results}>
@@ -36,6 +53,7 @@ export default function PopularMovies(props: { results: ResultPM[] }) {
           )}
         </For>
       </Swiper>
+      <div class={`absolute h-1 bg-orange-800 ease-linear ${twclass()}`}></div>
     </div>
   );
 }
