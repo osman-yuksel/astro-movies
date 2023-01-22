5928ace8f0db6108f4c6c1f086c3f79b993c14a2
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-22 08:19:24 +0300

    style changes

--- src/components/movies/MiniMovieCard.tsx
+++ src/components/movies/MiniMovieCard.tsx
@@ -1,40 +1,22 @@
 export default function Card(props: {
   id: number;
   title: string;
-  backdrop: string;
+  poster: string;
 }) {
   return (
-    <div class="relative flex h-32 justify-center m-2 overflow-hidden">
+    <div class="relative m-2 flex h-min max-w-max justify-center overflow-hidden rounded-md">
       <picture>
         <source
-          srcset={`https://image.tmdb.org/t/p/w342/${props.backdrop}`}
-          media="(min-width: 840px)"
-        />
-        <source
-          srcset={`https://image.tmdb.org/t/p/w185/${props.backdrop}`}
-          media="(min-width: 640px)"
-        />
-        <source
-          srcset={`https://image.tmdb.org/t/p/w342/${props.backdrop}`}
-          media="(min-width: 605px)"
-        />
-        <source
-          srcset={`https://image.tmdb.org/t/p/w185/${props.backdrop}`}
-          media="(min-width: 510px)"
-        />
-        <source
-          srcset={`https://image.tmdb.org/t/p/w154/${props.backdrop}`}
-          media="(min-width: 300px)"
+          srcset={`https://image.tmdb.org/t/p/w342${props.poster}`}
+          media="(min-width: 768px)"
         />
         <img
           alt={props.title}
           loading="eager"
-          src={`htps://image.tmdb.org/t/p/w92/${props.backdrop}`}
-          width="342"
-          height="556"
+          src={`https://image.tmdb.org/t/p/w185${props.poster}`}
+          style="min-height: 100%; min-width: 100%; object-fit: cover;"
         />
       </picture>
-      <h3 class="absolute z-10 text-white">{props.title}</h3>
     </div>
   );
 }
