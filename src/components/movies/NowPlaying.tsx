ac0485cebe7319524c262ed8acf5d769338b2135
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-22 08:19:12 +0300

    fix: prop corrected as poster

--- src/components/movies/NowPlaying.tsx
+++ src/components/movies/NowPlaying.tsx
@@ -4,14 +4,10 @@ import Card from "./MiniMovieCard";
 
 export default function NowPlaying(props: { results: ResultNP[] }) {
   return (
-    <div class="grid grid-cols-4">
+    <div class="grid w-full grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
       <For each={props.results}>
         {(movie, i) => (
-          <Card
-            title={movie.title}
-            id={movie.id}
-            backdrop={movie.backdrop_path}
-          />
+          <Card title={movie.title} id={movie.id} poster={movie.poster_path} />
         )}
       </For>
     </div>
