535026afc32f359883949fa0918eb6856520c4e5
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-23 18:54:16 +0300

    change: style & hover anim

--- src/components/movies/MiniMovieCard.tsx
+++ src/components/movies/MiniMovieCard.tsx
@@ -4,7 +4,7 @@ export default function Card(props: {
   poster: string;
 }) {
   return (
-    <div class="relative m-2 flex h-min max-w-max justify-center overflow-hidden rounded-md">
+    <div class="relative m-2 flex h-min max-w-max justify-center overflow-hidden rounded-md border-2 border-black transition-all hover:z-10 hover:scale-105 hover:border-gray-800">
       <picture>
         <source
           srcset={`https://image.tmdb.org/t/p/w342${props.poster}`}
@@ -17,6 +17,7 @@ export default function Card(props: {
           style="min-height: 100%; min-width: 100%; object-fit: cover;"
         />
       </picture>
+      <a class="absolute z-10 h-full w-full" href={`/movie/${props.id}`}></a>
     </div>
   );
 }
