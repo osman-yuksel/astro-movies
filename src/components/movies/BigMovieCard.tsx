c952bb146e669b44a3609f4da56c49b9154bea54
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-25 22:59:48 +0300

    fix: less margin between sections

--- src/components/movies/BigMovieCard.tsx
+++ src/components/movies/BigMovieCard.tsx
@@ -7,8 +7,8 @@ export default function Card(props: {
   rating: number;
 }) {
   return (
-    <div class="relative flex h-[28rem] w-full items-center text-white sm:h-[45rem]">
-      <div class="z-10 ml-4 mb-24 flex h-full w-full items-end sm:ml-14 sm:mb-0 sm:w-4/12 sm:items-center">
+    <div class="relative flex h-[26rem] w-full items-center text-white sm:h-[45rem]">
+      <div class="z-10 ml-4 mb-8 flex h-full w-full items-end sm:ml-14 sm:mb-0 sm:w-4/12 sm:items-center">
         <div>
           <h1 class="mb-4 w-full text-3xl sm:text-5xl">{props.title}</h1>
           <div class="mb-4 flex h-12 flex-row items-center text-xl">
@@ -47,17 +47,17 @@ export default function Card(props: {
         <div class="absolute h-full w-full bg-gradient-to-t from-black via-black/40 to-black/0 sm:bg-gradient-to-r" />
         <picture class="flex h-full justify-end">
           <source
-            data-srcset={`https://image.tmdb.org/t/p/w1280/${props.backdrop}`}
+            data-srcset={`https://image.tmdb.org/t/p/w1280${props.backdrop}`}
             media="(min-width: 780px)"
           />
           <source
-            data-srcset={`https://image.tmdb.org/t/p/w780/${props.backdrop}`}
+            data-srcset={`https://image.tmdb.org/t/p/w780${props.backdrop}`}
             media="(min-width: 300px)"
           />
           <img
             alt={props.title}
             style="min-height: 100%; min-width: 100%; object-fit: cover;"
-            data-src={`https://image.tmdb.org/t/p/w300/${props.backdrop}`}
+            data-src={`https://image.tmdb.org/t/p/w300${props.backdrop}`}
             class="swiper-lazy"
           />
           <div class="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
